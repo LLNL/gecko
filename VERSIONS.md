7ccfa1f61f006967bb72b2dd8a5b0077d4fc488b
lindstro <pl@llnl.gov>
2020-01-20 23:29:42 -0800

    Release 1.0.0

--- VERSIONS.md
+++ VERSIONS.md
@@ -1,5 +1,5 @@
 # Gecko Release Notes
 
-## 1.0.0 (January XX, 2020)
+## 1.0.0 (January 20, 2020)
 
 - Initial release under BSD license.
--- src/version.cpp
+++ src/version.cpp
@@ -1,4 +1,4 @@
 #include "gecko.h"
 
 const unsigned int Gecko::version = GECKO_VERSION;
-const char* const Gecko::version_string = "gecko version " GECKO_VERSION_STRING " (January XX, 2020)";
+const char* const Gecko::version_string = "gecko version " GECKO_VERSION_STRING " (January 20, 2020)";
